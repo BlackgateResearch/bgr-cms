b5f7d799fb4308e1668e4e3e540e90e41da11b1c
Tristram Oaten <tristram@oaten.name>
2013-01-29 15:20:00 +0000

    staticfiles

--- bgr_cms/settings.py
+++ bgr_cms/settings.py
@@ -57,7 +57,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = "%s/staticfiles" % os.getcwd()
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -65,7 +65,7 @@ STATIC_URL = '/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
-    "%s/staticfiles" % os.getcwd(),
+    '',
 )
 
 # List of finder classes that know how to find static files in
